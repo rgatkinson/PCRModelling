87382b2d0332ad1e1ef6eeffe9596f0521cc29fa
Robert Atkinson <bob@theatkinsons.org>
2016-01-21 14:28:06 -0800

    check point

--- Mathematica/boilerplate.h
+++ Mathematica/boilerplate.h
@@ -5,8 +5,23 @@
 //
 #pragma once
 
+#include "windows.h"
 #include "WolframLibrary.h"
 #include "memory.h"
+#include "strsafe.h"
+
+void Trace(LPSTR format, ...)
+    {
+    char message[256];
+    va_list args = NULL;
+    va_start(args, format);
+    StringCchVPrintfA(message, 256, format, args);
+
+    char buffer[256];
+    StringCchCopyA(buffer, 256, "ode: ");
+    StringCchCatA(buffer, 256, message);
+    OutputDebugStringA(buffer);
+    }
 
 EXTERN_C DLLEXPORT mint WolframLibrary_getVersion() 
     {
@@ -67,15 +82,32 @@ ODESTATE runODECore(const ODESTATE& inits, const ODEPARAMETERS & params, double 
     return *pStateCur;
     }
     
-void copyFromTensor(WolframLibraryData libData, mint count, double* pDest, MTensor tensor)
+mint arraySize(WolframLibraryData libData, MTensor tensor)
+    {
+    mint const* dims = libData->MTensor_getDimensions(tensor);
+    return dims[0];
+    }
+void copyFromTensor(WolframLibraryData libData, double* pDest, mint cDest, MTensor tensor)
     {
     double* pSource = libData->MTensor_getRealData(tensor);
-    memcpy(pDest, pSource, count * sizeof(double));
+
+    mint cSrc = arraySize(libData, tensor);
+    mint cb = min(cDest, cSrc) * sizeof(double);
+    mint cbDest = cDest * sizeof(double);
+
+    memcpy(pDest, pSource, cb);
+    memset(&pDest[cSrc], 0, cbDest - cb);
     }
-void copyToTensor(WolframLibraryData libData, mint count, MTensor tensor, double* pSource)
+void copyToTensor(WolframLibraryData libData, MTensor tensor, double* pSource, mint cSrc)
     {
     double* pDest = libData->MTensor_getRealData(tensor);
-    memcpy(pDest, pSource, count * sizeof(double));
+
+    mint cDest = arraySize(libData, tensor);
+    mint cb = min(cDest, cSrc) * sizeof(double);
+    mint cbDest = cDest * sizeof(double);
+
+    memcpy(pDest, pSource, cb);
+    memset(&pDest[cSrc], 0, cbDest - cb);
     }
 
 // Parameter order:
@@ -91,17 +123,19 @@ EXTERN_C DLLEXPORT int runODE(WolframLibraryData libData, mint argc, MArgument *
     mint rankState  = (mint)(sizeof(ODESTATE)      / sizeof(double));
     mint rankParams = (mint)(sizeof(ODEPARAMETERS) / sizeof(double));
 
+    Trace("state:%d params:%d", rankState, rankParams);
+
     ODESTATE inits;
     ODEPARAMETERS parameters;
 
-    copyFromTensor(libData, rankState, (double*)&inits, tensorInits);
-    copyFromTensor(libData, rankParams, (double*)&parameters, tensorParams);
+    copyFromTensor(libData, (double*)&inits, rankState, tensorInits);
+    copyFromTensor(libData, (double*)&parameters, rankParams, tensorParams);
     
     ODESTATE result = runODECore(inits, parameters, duration);
 
     MTensor tensorResult;
     libData->MTensor_new(MType_Real, 1, &rankState, &tensorResult);
-    copyToTensor(libData, rankState, tensorResult, (double*)&result);
+    copyToTensor(libData, tensorResult, (double*)&result, rankState);
 
     MArgument_setMTensor(aResult, tensorResult);
 
