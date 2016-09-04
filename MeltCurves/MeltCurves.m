d19e19e35add5f22161dc943c8365bac53d70ed1
Robert Atkinson <bob@theatkinsons.org>
2016-09-04 12:44:33 -0700

    initial project creation

---
+++ MeltCurves/MeltCurves.m
@@ -0,0 +1,22 @@
+(* Mathematica Package *)
+(* Created by Mathematica Plugin for IntelliJ IDEA *)
+
+(* :Title: MeltCurves *)
+(* :Context: MeltCurves` *)
+(* :Author: Robert Atkinson *)
+(* :Date: 2016-09-04 *)
+
+(* :Package Version: 0.1 *)
+(* :Mathematica Version: *)
+(* :Copyright: (c) 2016 bob *)
+(* :Keywords: *)
+(* :Discussion: *)
+
+BeginPackage["MeltCurves`"]
+(* Exported symbols added here with SymbolName::usage *)
+
+Begin["`Private`"]
+
+End[] (* `Private` *)
+
+EndPackage[]
