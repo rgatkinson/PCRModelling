498cf688143320a0a3370e76898c0fe14857723e
Robert Atkinson <bob@theatkinsons.org>
2016-11-14 13:54:13 -0800

    as initially created by Eclipse

---
+++ Workbench/MeltCurves/MeltCurves.m
@@ -0,0 +1,16 @@
+(* Wolfram Language Package *)
+
+(* Created by the Wolfram Workbench Nov 14, 2016 *)
+
+BeginPackage["MeltCurves`"]
+(* Exported symbols added here with SymbolName::usage *) 
+
+Begin["`Private`"]
+(* Implementation of the package *)
+
+
+
+End[]
+
+EndPackage[]
+
